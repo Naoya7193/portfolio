9a16dfb252d7e53a5b3bc4a4b8091a90650b5849
Naoya Fujimoto <soccer.ikimono.711@gmail.com>
2022-04-21 15:05:12 +0900

    20220421_15:04 TOPページ完成

---
+++ inc/footer.php
@@ -0,0 +1,4 @@
+<!-- copylight -->
+<div class="copylight">©︎2022 Naoya Fujimoto</div>
+
+</body>
---
+++ inc/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+    <!-- 文字コード -->
+    <meta charset="UTF-8">
+
+    <!-- IEバグ回避用 -->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- 説明文 -->
+    <meta name="description" content="">
+
+    <!-- ビューポート -->
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- OGP -->
+    <meta property="og:title" content="">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:url" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:image" content="">
+
+    <!-- Twitter OGP -->
+    <meta name="twitter:card" content="summary_large_image">
+
+    <!-- ファビコン -->
+    <link rel="" href="">
+
+    <!-- Webフォント -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+    <!-- アイコンフォント -->
+    <link href="https://fonts.googleapis.com/css2?family=Material+Icons" rel="stylesheet">
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@2.0.2/destyle.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://use.typekit.net/qgw0lcp.css">
+</head>
+
+<body>
+
+<!-- id="top" -->
+<div class="top" id="top"></div>
+
+<!-- 固定ナビ -->
+<div class="sidebar">
+    <a class="sidebar_logo" href="/"></a>
+    <nav class="sidebar_nav">
+        <ul>
+            <li><a href="/">HOME</a></li>
+            <li><a href="/skill">SKILL</a></li>
+            <li><a href="/works">WORKS</a></li>
+            <li><a href="/about">ABOUT</a></li>
+        </ul>
+    </nav>
+</div>
+
+
+
+<!-- トップに戻る -->
+<div class="return_top wrapper"><a href="#top"></a></div>
